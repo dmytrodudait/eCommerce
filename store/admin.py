6fdfaa663c52050faf9c7ad3b64e698fb5d93d2a
dmytrodudait <dmytrodudaua@gmail.com>
2022-10-06 16:41:42 +0300

    Add basket

--- store/admin.py
+++ store/admin.py
@@ -7,7 +7,8 @@ from .models import Category, Product
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
-    
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'slug', 'price',
